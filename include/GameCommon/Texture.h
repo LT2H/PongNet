3d7afa25cee84f1a3cf8225c5b63bb126a9e77dd
LT2H <letranhuuhung@gmail.com>
2025-05-14 17:40:10 +0700

    feat: finish Texture class

---
+++ include/GameCommon/Texture.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Common.h"
+#include "GameCommon/Common.h"
+#include <vector>
+
+namespace gm
+{
+// Texture2D is able to store and configure a texture in OpenGL.
+// It also hosts utility functions for easy management.
+class Texture2D
+{
+  public:
+    Texture2D();
+
+    void generate(unsigned int width, unsigned int height,
+                  std::vector<unsigned char>& data);
+
+    void bind() const;
+
+  private:
+    // Holds the ID of the texture object, used for all texture operations to
+    // reference to this particular texture
+    unsigned int id_{};
+
+    // Texture image dimensions, width and height of loaded image in pixels
+    unsigned int width_{ 0 };
+    unsigned int height_{ 0 };
+
+    // Texture Format
+    unsigned int internal_format_{ GL_RGB }; // format of texture object
+    unsigned int image_format_{ GL_RGB };    // format of loaded image
+
+    // Texture configuration
+    unsigned int wrap_s_{ GL_REPEAT }; // wrapping mode on S axis
+    unsigned int wrap_t_{ GL_REPEAT }; // wrapping mode on T axis
+    unsigned int filter_min_{
+        GL_LINEAR
+    }; // filtering mode if texture pixels < screen pixels
+    unsigned int filter_max_{
+        GL_LINEAR
+    }; // filtering mode if texture pixels > screen pixels
+};
+} // namespace gm
---
+++ src/Texture.cpp
@@ -0,0 +1,35 @@
+#include <GameCommon/Texture.h>
+#include <GameCommon/Common.h>
+
+gm::Texture2D::Texture2D() {}
+
+
+void gm::Texture2D::generate(unsigned int width, unsigned int height,
+                             std::vector<unsigned char>& data)
+{
+    width_  = width;
+    height_ = height;
+
+    // Create Texture
+    glBindTexture(GL_TEXTURE_2D, id_);
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 internal_format_,
+                 width,
+                 height,
+                 0,
+                 image_format_,
+                 GL_UNSIGNED_BYTE,
+                 data.data());
+
+    // set Texture wrap and filter modes
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap_s_);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap_t_);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter_min_);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter_max_);
+
+    // Unbind texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void gm::Texture2D::bind() const { glBindTexture(GL_TEXTURE_2D, id_); }
