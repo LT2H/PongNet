ac9d325780ca83d9f4d31a7caefb54a2ccb315e0
LT2H <letranhuuhung@gmail.com>
2025-05-14 17:39:53 +0700

    feat: finish Shader class

--- include/GameCommon/Shader.h
+++ include/GameCommon/Shader.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <glad/glad.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include "Common.h"
 
 namespace gm
@@ -21,26 +17,26 @@ class Shader
                      nullptr); // note: geometry source code is optional
 
     // utils
-    void set_float(const char* name, float value, bool use_shader = false);
-    void set_integer(const char* name, int value, bool use_shader = false);
-    void set_vector2f(const char* name, float x, float y, bool use_shader = false);
-    void set_vector2f(const char* name, const glm::vec2& value,
+    void set_float(std::string_view name, float value, bool use_shader = false);
+    void set_integer(std::string_view name, int value, bool use_shader = false);
+    void set_vector2f(std::string_view name, float x, float y, bool use_shader = false);
+    void set_vector2f(std::string_view name, const glm::vec2& value,
                       bool use_shader = false);
-    void set_vector3f(const char* name, float x, float y, float z,
+    void set_vector3f(std::string_view name, float x, float y, float z,
                       bool use_shader = false);
-    void set_vector3f(const char* name, const glm::vec3& value,
+    void set_vector3f(std::string_view name, const glm::vec3& value,
                       bool use_shader = false);
-    void set_vector4f(const char* name, float x, float y, float z, float w,
+    void set_vector4f(std::string_view name, float x, float y, float z, float w,
                       bool use_shader = false);
-    void set_vector4f(const char* name, const glm::vec4& value,
+    void set_vector4f(std::string_view name, const glm::vec4& value,
                       bool use_shader = false);
-    void set_matrix4(const char* name, const glm::mat4& matrix,
+    void set_matrix4(std::string_view name, const glm::mat4& matrix,
                      bool use_shader = false);
 
     unsigned int id{};
 
   private:
     // checks if compilation or linking failed and if so, print the error logs
-    void check_compile_errors(unsigned int object, std::string& type);
+    void check_compile_errors(unsigned int object, std::string_view type);
 };
 } // namespace gm
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,10 +1,13 @@
 #include <GameCommon/Shader.h>
 
 #include <GameCommon/Common.h>
+#include <cstddef>
+#include <iostream>
+#include "Shader.h"
 
 gm::Shader& gm::Shader::use()
 {
-    glUseProgram(this->id);
+    glUseProgram(id);
     return *this;
 }
 
@@ -20,4 +23,159 @@ void gm::Shader::compile(std::string_view vertex_source,
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_source.data(), nullptr);
     glCompileShader(fragment_shader);
+    check_compile_errors(vertex_shader, "VERTEX");
+
+    // Fragment shader
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_source.data(), nullptr);
+    glCompileShader(fragment_shader);
+    check_compile_errors(fragment_shader, "FRAGMENT");
+
+    // If geometry shader source code is given, also compile geo shader
+    if (geometry_source != nullptr)
+    {
+        geo_shader = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geo_shader, 1, &geometry_source.data(), nullptr);
+        check_compile_errors(geo_shader, "GEOMETRY");
+    }
+
+    // Shader program
+    id = glCreateProgram();
+    glAttachShader(id, vertex_shader);
+    glAttachShader(id, fragment_shader);
+    if (geometry_source != nullptr)
+    {
+        glAttachShader(id, geo_shader);
+    }
+
+    glLinkProgram(id);
+    check_compile_errors(id, "PROGRAM");
+
+    // Delete the shaders as they're linked into our program now and no longer
+    // necessary
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    if (geometry_source != nullptr)
+    {
+        glDeleteShader(geo_shader);
+    }
+}
+
+void gm::Shader::set_float(std::string_view name, float value, bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniform1f(glGetUniformLocation(id, name.data()), value);
+}
+
+void gm::Shader::set_integer(std::string_view name, int value, bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniform1f(glGetUniformLocation(id, name.data()), value);
+}
+
+void gm::Shader::set_vector2f(std::string_view name, float x, float y,
+                              bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniform2f(glGetUniformLocation(id, name.data()), x, y);
+}
+
+void gm::Shader::set_vector2f(std::string_view name, const glm::vec2& value,
+                              bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniform2f(glGetUniformLocation(id, name.data()), value.x, value.y);
+}
+
+void gm::Shader::set_vector3f(std::string_view name, float x, float y, float z,
+                              bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniform3f(glGetUniformLocation(id, name), x, y, z);
+}
+
+void gm::Shader::set_vector3f(std::string_view name, const glm::vec3& value,
+                              bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniform3f(glGetUniformLocation(id, name), value.x, value.y, value.z);
+}
+
+void gm::Shader::set_vector4f(std::string_view name, float x, float y, float z,
+                              float w, bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniform4f(glGetUniformLocation(id, name), x, y, z, w);
+}
+
+void gm::Shader::set_vector4f(std::string_view name, const glm::vec4& value,
+                              bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniform4f(glGetUniformLocation(id, name), value.x, value.y, value.z, value.w);
+}
+void gm::Shader::set_matrix4(std::string_view name, const glm::mat4& matrix,
+                             bool use_shader)
+{
+    if (use_shader)
+    {
+        use();
+    }
+    glUniformMatrix2x4fv(glGetUniformLocation(id, name), glm::value_ptr(matrix));
+}
+
+void gm::Shader::check_compile_errors(unsigned int object, std::string_view type)
+{
+    int success{};
+    std::array<char, 1024> info_log{};
+    if (type != "PROGRAM")
+    {
+        glGetShaderiv(object, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(object, 1024, nullptr, info_log.data());
+            std::cerr
+                << "| ERROR::SHADER: Compile-time error: Type: " << type << "\n"
+                << info_log.data()
+                << "\n -- --------------------------------------------------- -- "
+                << std::endl;
+        }
+    }
+    else
+    {
+        glGetProgramiv(object, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(object, 1024, nullptr, info_log.data());
+            std::cerr
+                << "| ERROR::Shader: Link-time error: Type: " << type << "\n"
+                << info_log.data()
+                << "\n -- --------------------------------------------------- -- "
+                << std::endl;
+        }
+    }
 }
