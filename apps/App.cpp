14a78c8aff869d2b4c3cf9407db123373ce9a5ec
LT2H <letranhuuhung@gmail.com>
2025-05-15 14:11:00 +0700

    feat: implement minimal startup code

--- apps/App.cpp
+++ apps/App.cpp
@@ -1,16 +1,19 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "GameCommon/Game.h"
 
 #include <GameCommon/Shader.h>
+#include <GameCommon/Texture.h>
+#include <GameCommon/ResourceManager.h>
 
 #include <GameCommon/Common.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+constexpr unsigned int SCR_WIDTH{ 800 };
+constexpr unsigned int SCR_HEIGHT{ 600 };
+
+gc::Game game(SCR_WIDTH, SCR_HEIGHT);
 
 int main()
 {
@@ -24,40 +27,73 @@ int main()
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
+    glfwWindowHint(GLFW_RESIZABLE, false);
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+    GLFWwindow* window{ glfwCreateWindow(
+        SCR_WIDTH, SCR_HEIGHT, "Breakout", nullptr, nullptr) };
+    if (!window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }    
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // OpenGL configuration
+    // --------------------
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    game.init();
+
+    // Delta time vars
+    double delta_time{ 0.0f };
+    double last_frame{ 0.0f };
 
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
-        processInput(window);
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
+        // calculate delta time
+        double current_frame{ glfwGetTime() };
+        delta_time = current_frame - last_frame;
+        last_frame = current_frame;
         glfwPollEvents();
+        
+        // manage user input
+        // -----------------
+        game.process_input(delta_time);
+
+        // update game state
+        // -----------------
+        game.update(delta_time);
+
+        // render
+        // ------
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        game.render();
+
+        glfwSwapBuffers(window);
     }
+
+    // delete all resources as loaded using the resource manager
+    // ---------------------------------------------------------
+    gm::ResourceManager::clear();
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -65,19 +101,30 @@ int main()
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// process all input: query GLFW whether relevant keys are pressed/released this
+// frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    // when a user presses the escape key, we set the WindowShouldClose property to
+    // true, closing the application
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+            game.keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            game.keys[key] = false;
+    }
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
+    // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
