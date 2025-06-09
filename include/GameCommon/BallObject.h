8ee55c0059adc23f10599a3a05ee7e9a4eb41150
LT2H <letranhuuhung@gmail.com>
2025-06-09 17:48:16 +0700

    wip: add basic boucing ball logic

---
+++ include/GameCommon/BallObject.h
@@ -0,0 +1,20 @@
+#include "Common.h"
+#include "GameObject.h"
+#include "Texture.h"
+
+namespace gc
+{
+class BallObject : public GameObject
+{
+  public: // ball state
+    float radius_;
+    bool stuck_;
+
+    explicit BallObject();
+    BallObject(const glm::vec2& pos, float radius, const glm::vec2& velocity,
+               const Texture2D& sprite);
+
+    glm::vec2 move(float dt, u32 window_width);
+    void reset(const glm::vec2& position, const glm::vec2& velocity);
+};
+} // namespace gc
---
+++ src/GameCommon/BallObject.cpp
@@ -0,0 +1,53 @@
+#include <GameCommon/BallObject.h>
+#include <GameCommon/Common.h>
+#include "GameCommon/GameObject.h"
+
+gc::BallObject::BallObject() : GameObject{}, radius_{ 12.5f }, stuck_{ true } {}
+
+gc::BallObject::BallObject(const glm::vec2& pos, float radius,
+                           const glm::vec2& velocity, const Texture2D& sprite)
+    : GameObject{ pos,
+                  glm::vec2{ radius * 2.0, radius * 2.0 },
+                  sprite,
+                  glm::vec3{ 1.0f },
+                  velocity },
+      radius_{ radius }, stuck_{ true }
+{
+}
+
+glm::vec2 gc::BallObject::move(float dt, u32 window_width)
+{
+    // if not stuck to player board 
+    if (!stuck_)
+    {
+        // move the ball
+        pos_ += velocity_ * dt;
+
+        // check if outside window bounds; if so, reverse velocity and restore at
+        // correct pos
+        if (pos_.x <= 0.0f)
+        {
+            velocity_.x = -velocity_.x;
+            pos_.x      = 0.0f;
+        }
+        else if (pos_.x + size_.x >= window_width)
+        {
+            velocity_.x = -velocity_.x;
+            pos_.x      = window_width - size_.x;
+        }
+        if (pos_.y <= 0.0f)
+        {
+            velocity_.y = -velocity_.y;
+            pos_.y      = 0.0f;
+        }
+    }
+    return pos_;
+}
+
+// resets the ball to initial stuck position (if ball is outside window bounds)
+void gc::BallObject::reset(const glm::vec2& position, const glm::vec2& velocity)
+{
+    pos_      = position;
+    velocity_ = velocity;
+    stuck_    = true;
+}
