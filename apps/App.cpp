62a7952f225520ea3d2c73c1278609b7f9770ca0
LT2H <letranhuuhung@gmail.com>
2025-06-13 17:34:01 +0700

    chore: minor naming changes

--- apps/App.cpp
+++ apps/App.cpp
@@ -113,9 +113,9 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     if (key >= 0 && key < 1024)
     {
         if (action == GLFW_PRESS)
-            game.keys[key] = true;
+            game.keys_[key] = true;
         else if (action == GLFW_RELEASE)
-            game.keys[key] = false;
+            game.keys_[key] = false;
     }
 }
 
